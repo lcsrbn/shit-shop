c1187cc60831f53cb744bca4911dc71e8a79d0c0
luca <lcsrbn@users.noreply.github.com>
2026-03-08 14:54:10 +0100

    fix root layout provider structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,8 @@ export default function RootLayout({
   return (
     <html lang="it">
       <body>
-        <Header />
-        {children}
         <CartProvider>
+          <Header />
           {children}
           <SiteChrome />
         </CartProvider>
